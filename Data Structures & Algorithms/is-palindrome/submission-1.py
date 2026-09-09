da5e969968b87c243fd35e79063349aa56242997
Francesco Siciliani <francescosiciliani624@gmail.com>
2026-09-09 06:25:47 +0200

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        i=0
+        j=len(s)-1
+
+        while i<j:
+            if(not s[i].isalnum()):
+                i+=1
+            elif(not s[j].isalnum()):
+                j-=1
+            elif(s[i].casefold()==s[j].casefold()):
+                i+=1
+                j-=1
+            else:
+                return False
+        return True
+      
+       
+
+
+            
+        
